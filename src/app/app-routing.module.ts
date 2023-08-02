754b9aa5f3551ac4f46339f2c49b1459d7ea7083
XiomaraPulido <xiomaraa.pulido@gmail.com>
2023-08-02 14:40:48 -0500

    Lazy loading Routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'homePage',
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'favouritesPage',
+    loadChildren: () => import('./modules/favourites/favourites.module').then(m => m.FavouritesModule)
+  },
+  {
+    path: '',
+    redirectTo: 'homePage',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
