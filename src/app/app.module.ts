544c0016b8b91e483453f1262d1d5094f220c254
XiomaraPulido <xiomaraa.pulido@gmail.com>
2023-08-02 14:35:45 -0500

    Initial structure Home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FavouritesCryptocurrenciesComponent } from './modules/components/favourites-cryptocurrencies/favourites-cryptocurrencies.component';
+import { LayoutComponent } from './modules/components/layout/layout.component';
+import { ListCryptocurrenciesComponent } from './modules/components/list-cryptocurrencies/list-cryptocurrencies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavouritesCryptocurrenciesComponent,
+    LayoutComponent,
+    ListCryptocurrenciesComponent
   ],
   imports: [
     BrowserModule,
