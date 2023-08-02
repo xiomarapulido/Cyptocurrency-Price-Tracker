e648158a53bd638672175b2eddb0bcc0a412979f
XiomaraPulido <xiomaraa.pulido@gmail.com>
2023-08-02 14:38:41 -0500

    Initial structure Favourite  module

---
+++ src/app/modules/favourites/components/edit-favourites/edit-favourites.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-favourites',
+  templateUrl: './edit-favourites.component.html',
+  styleUrls: ['./edit-favourites.component.scss']
+})
+export class EditFavouritesComponent {
+
+}
